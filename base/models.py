ff42506bfd1e4633ff6171c989e3b21ad95339be
Matthew James Lyons <matthew@Matthews-Air.lan>
2023-11-15 11:10:07 -0600

    Set up student/practice models

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models 
-
+from django.apps import apps
 
 
 class User(AbstractUser):
@@ -9,4 +9,15 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
     email = models.EmailField(null=True)
-    
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            print('saving...')
+            super(User, self).save(*args, **kwargs)
+            print('saved')
+            if self.student_or_teacher == 'student':
+                Student = apps.get_model('student', 'Student')
+                user = self
+                Student.objects.create(user=user, name=self.username)
+        else:
+            super(User, self).save(*args, **kwargs)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from base.models import User
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Student(models.Model):
+    User = get_user_model()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    name = models.CharField(max_length=50, default=user)
+    joined = models.TimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.name
+class PracticeEntry(models.Model):
+    # Practice time in minutes
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    time = models.IntegerField(validators=[MaxValueValidator(1440)])
+    notes = models.TextField(max_length=500)
+    created = models.TimeField(auto_now_add=True, editable=False)
+    modified = models.TimeField(auto_now=True)
